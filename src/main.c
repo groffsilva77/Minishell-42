09a108def926d77fd4ae3867f69c91b6f48954bf
Gabriel Groff Da Silva <ggroff-d@c3r10p3.42sp.org.br>
2025-01-28 14:38:13 -0300

    modified main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ggroff-d <ggroff-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 14:14:15 by ggroff-d          #+#    #+#             */
-/*   Updated: 2025/01/22 17:18:44 by ggroff-d         ###   ########.fr       */
+/*   Updated: 2025/01/28 14:20:00 by ggroff-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,38 +78,3 @@ int	main(void)
 	shell_loop();
 	return (0);
 }
-
-// int	main(void)
-// {
-// 	t_token	*tokens;
-// 	t_token	*current;
-// 	char	*input;
-// 	char	*expanded_value;
-
-// 	while (1)
-// 	{
-// 		input = readline("minishell> ");
-// 		if (!input)
-// 			break ;
-// 		if (*input)
-// 			add_history(input);
-// 		tokens = tokenize(input);
-// 		if (!validate_syntax(tokens))
-// 		{
-// 			free_tokens(tokens);
-// 			free(input);
-// 			continue ;
-// 		}
-// 		current = tokens;
-// 		while (current)
-// 		{
-// 			expanded_value = expand_tokens(current->value);
-// 			printf("Token expandido: '%s'\n", expanded_value);
-// 			free(expanded_value);
-// 			current = current->next;
-// 		}
-// 		free_tokens(tokens);
-// 		free(input);
-// 	}
-// 	return (0);
-// }
